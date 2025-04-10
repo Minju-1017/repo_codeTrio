2af51f6d18c526c4e342ee1109873e389f1b1150
JTY5606 <jty2298@gmail.com>
2025-04-10 16:03:04 +0900

    검색구분 수정

--- codetrio/src/main/java/com/codetrio/module/order/OrderVo.java
+++ codetrio/src/main/java/com/codetrio/module/order/OrderVo.java
@@ -6,6 +6,28 @@ public class OrderVo extends BaseVo {
 	
 	private String oSeq;
 	private String oStateCd;
+	private String opStateCd;
+	
+	
+	public String getOpStateCd() {
+		return opStateCd;
+	}
+
+	public void setOpStateCd(String opStateCd) {
+		this.opStateCd = opStateCd;
+	}
+
+	private Integer shStateOption;			// null 값을 받아야 되는 경우가 있어서 int 대신 Integer 사용
+	
+	
+	public Integer getShStateOption() {
+		return shStateOption;
+	}
+
+	public void setShStateOption(Integer shStateOption) {
+		this.shStateOption = shStateOption;
+	}
+
 
 	public String getoStateCd() {
 		return oStateCd;
